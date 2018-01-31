f06f3c24f3b015ce496869e777b8692ebc18c61b
Brian Contreras <briancontreras@DN0a223079.SUNet>
2018-01-31 14:14:13 -0800

    quake2

--- week-04/stanford_quakebot/quakebot.py
+++ week-04/stanford_quakebot/quakebot.py
@@ -1,11 +1,12 @@
-def calcDistance(lat1, lon1, lat2, lon2):
-    from math import sin, cos, sqrt, atan2, radians
-    R = 6373.0
+from math import sin, cos, sqrt, atan2, radians
+EARTH_RADIUS_IN_KM = 6373.0
 
-    lat1 = radians(lat1)
-    lon1 = radians(lon1)
-    lat2 = radians(lat2)
-    lon2 = radians(lon2)
+def calc_geo_distance(longitude_1, latitude_1, longitude_2, latitude_2):
+
+    lon1 = radians(longitude_1)
+    lat1 = radians(latitude_1)
+    lon2 = radians(longitude_2)
+    lat2 = radians(latitude_2)
 
     dlon = lon2 - lon1
     dlat = lat2 - lat1
@@ -13,6 +14,4 @@ def calcDistance(lat1, lon1, lat2, lon2):
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    distance = R * c
-
-    print("Result:", distance)
+    return EARTH_RADIUS_IN_KM * c
